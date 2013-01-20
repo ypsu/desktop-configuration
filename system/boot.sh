e7fefeb17ce1c56445b1fd9dd73f3d6a61abcb59
Balazs Kezes <rlblaster@gmail.com>
2013-01-20 21:23:51 +0100

    Add my system initialization/shutdown scripts

---
+++ system/boot.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+function logexec() {
+	echo "$@"
+	"$@"
+}
+
+function log() {
+	echo -e "\e[33m""$@""\e[0m"
+}
+
+hostname eper
+
+log Mounting system dirs
+logexec mount -t proc proc /proc -o nosuid,noexec,nodev
+logexec mount -t sysfs sys /sys -o nosuid,noexec,nodev
+logexec mount -t tmpfs run /run -o mode=0755,nosuid,nodev
+logexec mkdir -p /dev/pts /dev/shm
+logexec mount -t devpts devpts /dev/pts -o mode=0620,gid=5,nosuid,noexec
+logexec mount -t tmpfs shm /dev/shm -o mode=1777,nosuid,nodev
+logexec mount -t tmpfs tmpfs /tmp -o nosuid,nodev,size=128000k
+
+log Waiting for udev
+logexec udevd --daemon
+logexec udevadm trigger --action=add --type=subsystems
+logexec udevadm trigger --action=add --type=devices
+logexec udevadm settle
+
+log Bringing up networking
+logexec ifconfig lo up
+logexec ifconfig eth0 up
+logexec ifconfig eth0 192.168.1.123 netmask 255.255.255.0 broadcast 192.168.1.255
+logexec route add default gw 192.168.1.1 eth0
+
+log Checking filesystems
+logexec fsck -T -C /
+if test $? -ge 2; then
+	agetty -8 -a root --noclear 38400 tty1 linux
+fi
+
+log Mounting filesystems
+logexec mount -o remount,rw /
+logexec mount /boot
+
+log Miscellaneous settings
+echo Setting tty keymap
+(dumpkeys | grep -i keymaps; echo keycode 58 = Escape) | loadkeys - >/dev/null
+echo Setting up environment
+export LC_ALL=en_US.UTF-8
+export PATH=/home/rlblaster/.sbin:$PATH
+echo Setting font
+setfont -f /usr/share/kbd/consolefonts/ter-g12n.psf.gz
+logexec modprobe snd_bcm2835
+logexec alsactl restore
+echo Setting kernel variables
+echo 100 > /proc/sys/vm/dirty_background_ratio
+echo 100 > /proc/sys/vm/dirty_ratio
+echo 30000 > /proc/sys/vm/dirty_expire_centisecs
+echo 18000 > /proc/sys/vm/dirty_writeback_centisecs
+echo 16384 > /proc/sys/vm/min_free_kbytes
+
+log Starting X in the background
+echo 'startx &'
+cd ~rlblaster
+su -c "startx -- vt7 -nolisten tcp" rlblaster 2>/dev/null >&2 &
+cd - >/dev/null
+
+log Setting time
+sntp
+
+log Starting ttys
+agetty -8 -a rlblaster -o "-p -f rlblaster" --noclear 38400 tty2 linux &
+agetty -8 -a rlblaster -o "-p -f rlblaster" --noclear 38400 tty3 linux &
+agetty -8 -a rlblaster -o "-p -f rlblaster" --noclear 38400 tty4 linux &
+agetty -8 -a rlblaster -o "-p -f rlblaster" --noclear 38400 tty5 linux &
+agetty -8 -a rlblaster -o "-p -f rlblaster" --noclear 38400 tty6 linux &
+agetty -8 -a rlblaster -o "-p -f rlblaster" --noclear 38400 tty1 linux &
---
+++ system/halt.c
@@ -0,0 +1,109 @@
+#define _GNU_SOURCE
+#include <ctype.h>
+#include <dirent.h>
+#include <linux/reboot.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/reboot.h>
+#include <unistd.h>
+
+int mypid;
+
+// Returns true if there was no process available
+bool killall(int sig)
+{
+	bool found_process = false;
+
+	DIR *dir = opendir("/proc");
+	if (dir == NULL) abort();
+
+	struct dirent *dirent;
+	while ((dirent = readdir(dir)) != NULL) {
+		if (dirent->d_type != DT_DIR)
+			continue;
+		if (!isdigit(dirent->d_name[0]))
+			continue;
+		int pid = atoi(dirent->d_name);
+		if (pid == 1 || pid == mypid)
+			continue;
+		char exe[64];
+		sprintf(exe, "/proc/%d/exe", pid);
+		char link[4096];
+		if (readlink(exe, link, sizeof link) == -1)
+			continue;
+		kill(pid, sig);
+		found_process = true;
+	}
+
+	closedir(dir);
+	return !found_process;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 1) {
+		puts("????");
+		exit(2);
+	}
+
+	enum { CMD_REBOOT, CMD_POWEROFF } request;
+	if (strcmp(argv[0], "reboot") == 0) {
+		request = CMD_REBOOT;
+	} else if (strcmp(argv[0], "poweroff") == 0) {
+		request = CMD_POWEROFF;
+	} else {
+		puts("Please use either \"reboot\" or \"poweroff\"!");
+		exit(1);
+	}
+
+	if (geteuid() != 0) {
+		puts("Must be root to execute this!");
+		exit(1);
+	}
+	setuid(0);
+
+	freopen("/dev/console", "w", stdout);
+	freopen("/dev/console", "w", stderr);
+	setlinebuf(stdout);
+	setlinebuf(stderr);
+
+	system("chvt 1");
+	mypid = getpid();
+	for (int sig = 0; sig < 32; ++sig) {
+		signal(sig, SIG_IGN);
+	}
+
+	puts("Sending SIGTERM to the processes");
+	for (int i = 0; i < 5; ++i) {
+		if (killall(SIGTERM))
+			break;
+		sleep(1);
+	}
+	puts("Sending SIGKILL to the processes");
+	for (int i = 0; i < 5; ++i) {
+		if (killall(SIGKILL))
+			break;
+		sleep(1);
+	}
+
+	puts("Unmounting filesystems");
+	system("umount -a");
+	puts("Syncing");
+	sync();
+	puts("Remounting root");
+	system("mount -o remount,ro /");
+	sync();
+	if (request == CMD_REBOOT) {
+		puts("Rebooting");
+		sleep(1);
+		reboot(LINUX_REBOOT_CMD_RESTART);
+	} else if (request == CMD_POWEROFF) {
+		puts("Shutting down");
+		sleep(1);
+		reboot(LINUX_REBOOT_CMD_POWER_OFF);
+	}
+	return 0;
+}
