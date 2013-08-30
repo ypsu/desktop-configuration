ce762c4b6644534710ac494ef5e1c2132edb6288
Balazs Kezes <rlblaster@gmail.com>
2013-08-30 21:01:02 +0100

    Merge with master

--- system/sntp.c
+++ system/sntp.c
@@ -11,6 +11,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/time.h>
+#include <signal.h>
 
 /* This is the way to make TOSTRING(__LINE__) to work. */
 #define STRINGIFY(x) #x
@@ -26,8 +27,19 @@ unsigned char data[15*4];
 
 const long long NTP2UNIX = (70 * 365 + 17) * 86400LL;
 
+void sigalrm_handler(int sig)
+{
+	(void) sig;
+	puts("Could not get time for some reason, exiting.");
+	exit(1);
+}
+
 int main(void)
 {
+	if (signal(SIGALRM, sigalrm_handler) == SIG_ERR)
+		abort();
+	alarm(3);
+
 	int fd;
 	struct sockaddr_in sa;
 
@@ -37,7 +49,7 @@ int main(void)
 
 	sa.sin_family = AF_INET;
 	sa.sin_port = htons(PORT);
-	inet_pton(AF_INET, "148.6.0.1", &sa.sin_addr);
+	inet_pton(AF_INET, "85.91.1.180", &sa.sin_addr);
 
 	CHECK(sendto(fd, data, sizeof data, 0,
 			(struct sockaddr*) &sa, sizeof sa));
